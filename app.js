ee9a3026f7f822d767eadcdb900b3ccf49a052f5
Kevin <GIT SERVICE EMAIL>
2020-11-03 12:06:21 +1300

    changes to h1tag

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port=process.env.PORT || 3000
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello Kevin</h1>');
+    res.end('<h1>Mission Ready dev team is best team</h1>');
 });
 
 server.listen(port, () => {
